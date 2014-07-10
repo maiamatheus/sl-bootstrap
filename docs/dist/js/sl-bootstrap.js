eb31038c8b09b49cabbfbf48944aa3874f0583e7
Joshua Cochran <jcochran@softlayer.com>
2014-07-10 08:17:08 -0500

    Version bumped to 0.1.1.  Based off version 3.2.0+ of Twitter Bootstrap

--- docs/dist/js/sl-bootstrap.js
+++ docs/dist/js/sl-bootstrap.js
@@ -1,6 +1,6 @@
 /*!
- * Bootstrap v3.2.0 (http://getbootstrap.com)
- * Copyright 2011-2014 Twitter, Inc.
+ * Bootstrap v0.1.1 (https://gitlab.softlayer.local/interface/sl-bootstrap)
+ * Copyright 2011-2014 Twitter, Inc. / SoftLayer
  * Licensed under MIT (https://github.com/twbs/bootstrap/blob/master/LICENSE)
  */
 
